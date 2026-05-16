939d075541c4d25493a660d9341f55b47f8f6b59
Ronald Manzano <md23005@ues.edu.sv>
2026-05-16 13:56:50 -0600

    R2: Segundo commit, modificación de ajuste de pantalla para que muestre el título de la toolbar

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -10,13 +10,14 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro);
 
         //Obtener los botones
